6096e26296c694f272249f10ff74eb357982af6b
alexfs9 <Alexander_Flores_Solar@hotmail.com>
2024-07-22 12:27:54 -0500

    Base

---
+++ src/main/java/com/minimarket_api/MinimarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.minimarket_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minimarket_api/MinimarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minimarket_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
